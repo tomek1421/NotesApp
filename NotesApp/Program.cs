40716646285967331736ea777909673b73bbe444
tomek_1421 <t.wojciechowski.648@studms.ug.edu.pl>
2024-09-09 14:03:35 +0200

    add HomeController and connect to database v2

--- NotesApp/Program.cs
+++ NotesApp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NotesApp.Entities;
+
 namespace NotesApp;
 
 public class Program
@@ -5,10 +8,43 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        
+        //Services
+        builder.Services.AddControllers();
+        
+        //add DbContext as service
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+        
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
-
+        // Check database connection
+        using (var scope = app.Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            try
+            {
+                var canConnect = dbContext.Database.CanConnect();
+                if (canConnect)
+                {
+                    app.Logger.LogInformation("Successfully connected to the database.");
+                }
+                else
+                {
+                    app.Logger.LogError("Failed to connect to the database.");
+                }
+            }
+            catch (Exception ex)
+            {
+                app.Logger.LogError(ex, "An error occurred while trying to connect to the database.");
+            }
+        }
+        
+        app.UseRouting();
+        app.MapControllers();
+        
         app.Run();
     }
 }
