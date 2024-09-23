88abc66243b1109a3d99a8bcce9b2f1df0f7d251
tomek_1421 <t.wojciechowski.648@studms.ug.edu.pl>
2024-09-23 10:13:19 +0200

    add api calls for subject

---
+++ client/src/apiCalls/subjects.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const url = "http://localhost:5262/subjects"
+
+async function createSubject(body) {
+    return await axios.post(url, body);
+}
+
+async function deleteSubject(subjectId) {
+    return await axios.delete(`${url}/${subjectId}`);
+}
+
+async function getSubjectWithNotes(subjectId) {
+    return await axios.get(`${url}/${subjectId}/notes`);
+}
+
+async function updateSubject(subjectId, body) {
+    return await axios.put(`${url}/${subjectId}`, body);
+}
+
+export { createSubject, deleteSubject, getSubjectWithNotes, updateSubject };
