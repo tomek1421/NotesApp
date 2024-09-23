436bc30edd6c3706e63dbdd5778c3529d73c474d
tomek_1421 <t.wojciechowski.648@studms.ug.edu.pl>
2024-09-23 10:04:29 +0200

    Routes refactor

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import AllSubjectsPage from './pages/AllSubjectsPage';
-import AddSubjectPage from './operation_pages/AddSubjectPage';
-import DeleteSubjectPage from './operation_pages/DeleteSubjectPage';
-import SubjectPage from './pages/SubjectPage';
-import NotePage from './pages/NotePage';
-import AddNotePage from './operation_pages/AddNotePage';
-import DeleteNotePage from './operation_pages/DeleteNotePage';
-import UpdateSubjectPage from './operation_pages/UpdateSubjectPage';
 import TasksPage from './pages/TasksPage';
 import SemesterPlanPage from './pages/SemesterPlanPage';
 
@@ -21,15 +14,7 @@ function App() {
           <Route path="" element={<HomePage />} />
           <Route path="subjects">
             <Route index element={<AllSubjectsPage />}/>
-            <Route path="add-subject" element={<AddSubjectPage />} />
-            <Route path=":subjectId/delete-subject" element={<DeleteSubjectPage />} />
-            <Route path=":subjectId/edit" element={<UpdateSubjectPage />} />
-            <Route path=":subjectId/notes">
-              <Route index element={<SubjectPage />} />
-              <Route path=":noteId" element={<NotePage />} />
-              <Route path="add-note" element={<AddNotePage />} />
-              <Route path=":noteId/delete-note" element={<DeleteNotePage />} />
-            </Route>
+           
           </Route>
           <Route path="tasks" element={<TasksPage />} />
           <Route path="plan" element={<SemesterPlanPage />} />
